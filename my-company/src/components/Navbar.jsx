cc87896f655f349dda9e005683ff9ffa50d41ae9
Khaled ElBayoumi <158460490+Kbayoumi561@users.noreply.github.com>
2024-11-17 13:58:19 +0200

    Resolving inline styling issue

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav style={{ 
+            backgroundColor: '#f4f4f4', 
+            display: 'flex', 
+            justifyContent: 'space-around', 
+            padding: '10px' 
+        }}>
+            <Link to="/" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>Home</Link>
+            <Link to="/about" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>About</Link>
+            <Link to="/services" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>Services</Link>
+            <Link to="/contact" style={{ margin: '0 10px', textDecoration: 'none', color: 'black' }}>Contact</Link>
+        </nav>
+    );
+}
+
+export default Navbar;
