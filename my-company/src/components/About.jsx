01ec62de3daeb7ff21cb5b64cb4293eef0512a95
Khaled ElBayoumi <158460490+Kbayoumi561@users.noreply.github.com>
2024-11-17 13:02:07 +0200

    uploading the new project files

---
+++ my-company/src/components/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div style={{ padding: '20px' }}>
+            <h1>About Us</h1>
+            <p>We are a company committed to providing the best solutions for our clients.</p>
+        </div>
+    );
+}
+
+export default About;
