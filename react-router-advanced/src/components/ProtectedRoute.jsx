3ac0cab197e03402248b13c21b1f53d36392168f
Khaled ElBayoumi <158460490+Kbayoumi561@users.noreply.github.com>
2024-11-23 19:54:40 +0200

    uploading the new project with req

---
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ isAuthenticated, children }) => {
+    return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+ProtectedRoute.propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired,
+    children: PropTypes.node.isRequired,
+};
+
+export default ProtectedRoute;
